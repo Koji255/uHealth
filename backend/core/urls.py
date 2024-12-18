29b6482ab27808744d7f4004bb30f4acaf0ee22f
Koji255 <koji25ony1@gmail.com>
2024-12-18 08:22:25 +0300

    base html frontend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,5 +5,5 @@ from core.views import CreateAppointmentView
 app_name = 'core'
 
 urlpatterns = [
-    path('new_appointment/', CreateAppointmentView.as_view(), name='new-appointment'),
+    path('new-appointment/', CreateAppointmentView.as_view(), name='new-appointment'),
 ]
